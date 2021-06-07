c8722c868cb66f11d8d62f67495b40e562de5c74
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-06-07 23:02:00 +0000

    Update before template engine

--- express_server.js
+++ express_server.js
@@ -10,8 +10,9 @@ const urlDatabase = {
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.render('pages/index');
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 
