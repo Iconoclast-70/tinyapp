5d59dc7ae0dfddb555b27595510296ccff19ac96
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-06-08 01:38:55 +0000

    URL Shortening Part 1

--- express_server.js
+++ express_server.js
@@ -1,14 +1,28 @@
 const express = require("express");
-const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return Math.random().toString(36).replace('0.', '').slice(5);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.set('view engine', 'ejs');
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
